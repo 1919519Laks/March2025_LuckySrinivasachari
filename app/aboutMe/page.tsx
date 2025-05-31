432ec17b8b8698e96751721ea7e579b30495b2e0
1919519Laks <trustlaksalternate@gmail.com>
2025-05-30 18:06:02 -0700

    please work

--- app/aboutMe/page.tsx
+++ app/aboutMe/page.tsx
@@ -1,27 +1,160 @@
 "use client";
-import { title } from "@/components/primitives";
 import { Button } from "@heroui/button";
-import link from "next/link";
 import { useState } from "react";
 
+import { title } from "@/components/primitives";
+
 export default function AboutMePage() {
-  let facts: string[] = ["1. The digits 1-9 add up to 45, and this is called the 'Rule of 45' in sudoku", "2.  The modern Sudoku was most likely designed anonymously by Howard Garns" , "3. Variant sudoku competitions exist worldwide, organized by the World Puzzle Association." , "4. The \"German whispers\" variant sudoku rule was invented by Axel Gebhardt", "5. There exists a Variant sudoku puzzle so convoluted that only 2 people have been known to solve it - \"Word puzzle\". Can't list a link due to Securly", "fact 6", "fact 7", "act 8", "act 9", "fact 10"];
+  let facts: string[] = [
+    "n 2006, an Australian man tried to sell New Zealand on eBay. The price rose to $3,000 before eBay shut it down.",
+    "When asked if he knew the speed of sound, Einstein said he \"didn't carry such information in my mind since it's readily available in books.",
+    '12+1 = 11+2, and "twelve plus one" is an anagram of "eleven plus two."',
+    "William McKinley was on the $500 bill, Grover Cleveland was on the $1,000, and James Madison was on the $5,000.",
+    "In Japan, letting a sumo wrestler make your baby cry is considered good luck.",
+    "Nutella was invented during WWII, when an Italian pastry maker mixed hazelnuts into chocolate to extend his chocolate ration.",
+    "The Scots have a word for that panicky hesitation you get when introducing someone whose name you can't remember: tartle.",
+  ];
   const [fact, setFact] = useState("");
-  
+
   return (
     <div>
       <h1 className={title()}>About Me!</h1>
-      <Button onPress={() => {setFact(facts[Math.floor(Math.random()*10)])}}> Press for a fact</Button>
-      <br></br>
-      <p>I am a cryptid, there are no confirmed photos of meee✨</p>
-      <h2 className="text-center text-pretty size-48">Who am I and why should you care?</h2>
-      {/* I AM IN YOUR WALLS, BEAUTIFUL */}
-      <br /><br /><br /><br />
-      <p>{fact}</p>
-      <img src="art_dahlia.jpg" alt="Error" height={300} width={400}></img>
-      <Button as={link} href="https://issaquah.instructure.com/?login_success=1">Canvas</Button>
+
       <br />
-      <p>flexbox pracitce</p>
+      <br />
+      <br />
+
+      <p>I am a cryptid! There are no confirmed photos of meeee</p>
+      <a
+        color="blue"
+        href="https://www.w3schools.com/css/css3_flexbox_container.asp"
+      >
+        W3 schools!
+      </a>
+      <br />
+      <br />
+      <p>HOWever! I have a ton of photos of hobbies!</p>
+      <div className="flex align-around justify-evenly flex-wrap">
+        <div className="basis-1/5">
+          <img alt="bob ross etch a sketch" src="bobross.JPG" />
+          <p>
+            This is my Etch-A-Sketch Rendition of one of Bob Ross&apos;
+            paintings! I
+          </p>
+          <p>
+            have quite a bit of experience with the Etch A Sketch! I also have
+            other images here!
+          </p>
+        </div>
+        <div className="basis-1/5">
+          <img alt="campfire EAS" src="campfire.JPG" />
+          <p>
+            This project took me 2 days! The longest time I&apos;ve spent on
+            such a project
+          </p>
+        </div>
+        <div className="basis-1/5">
+          <img alt="birds etch" src="etchBird.jpg" />
+        </div>
+        <div className="basis-1/5">
+          <img alt="DnD town" src="harx.JPG" />
+        </div>
+        <div className="basis-1/5">
+          <img alt="reindeer etch" src="reindeer.JPG" />
+          <p>This was my first time touching an etch a sketch!</p>
+        </div>
+      </div>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <div className="flex align-around justify-evenly flex-wrap">
+        <div className="basis-1/3">
+          <p>
+            I also am a bit of a geek for pretty mushrooms! This is a Parrot Wax
+            Cap!
+          </p>
+          <img alt="parrotwax" src="parrotWax.JPG" />
+        </div>
+        <div className="basis-1/3">
+          <img alt="ruby bonnet" src="rubyyBonnet.PNG" />
+          <p>Look! Ruby bonnet! So sweet, so cute, so smoll. Such baby.</p>
+        </div>
+        <div className="basis-1/3">
+          <img alt="stinkhorn" src="veiledStinkhorn.PNG" />
+          <p>Veiled stinkhorn!</p>
+        </div>
+      </div>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <div className="flex align-around justify-evenly flex-wrap">
+        <div className="basis-1/2">
+          <p>I also do macrophotography!</p>
+          <img alt="clovers" src="clovermacro.JPEG" />
+        </div>
+      </div>
+      <br />
+      <br />
+      <br />
+      <div className="flex align-around justify-evenly flex-wrap">
+        <div className="basis-1/2">
+          <p>
+            Another passion of mine is Variant sudoku, which takes regular
+            sudoku and tosses in extra rules!
+          </p>
+          <p>These are some puzzles I made</p>
+          <img alt="dawn" src="dawn.PNG" />
+        </div>
+        <div className="basis-1/2">
+          <p>This is part of a gift set I gave to a friend!</p>
+          <img alt="The Monster" src="themonster.PNG" />
+        </div>
+      </div>
+
+      <br />
+      <br />
+      <br />
+      <div className="flex align-around justify-evenly flex-wrap">
+        <div className="basis-1/2">
+          <p>
+            Another pet project/goal of mine is to code Rock Paper Scissors; and
+            all its variants
+          </p>
+          <img alt="7" src="rps7.PNG" />
+        </div>
+        <div className="basis-1/2">
+          <img alt="11" src="rps11.PNG" />
+        </div>
+        <div className="basis-1/2">
+          <img alt="15" src="rps15.PNG" />
+        </div>
+        <div className="basis-1/2">
+          <img alt="101" src="rps101.PNG" />
+        </div>
+      </div>
+
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <Button
+        onPress={() => {
+          setFact(facts[Math.floor(Math.random() * facts.length)]);
+        }}
+      >
+        Press for a fact!
+      </Button>
+      <p>{fact}</p>
+
+       <p>flexbox pracitce</p>
       <div className="flex justify-between border-1 border-black p-4">
         {/* parent container */}
         <div className="m-4 border-5 border-green-700">
@@ -46,3 +179,4 @@ export default function AboutMePage() {
     </div>
   );
 }
+
